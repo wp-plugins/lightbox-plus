511389bf081bf3310be421dbb56bfef41d335864
dzappone <dzappone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-27 18:59:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/lightbox-plus@105752 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/lightboxplus.php
+++ trunk/lightboxplus.php
@@ -5,8 +5,9 @@ Plugin URI: http://www.23systems.net/plugins/lightbox-plus/
 Description: Lightbox permits users to view larger versions of images without having to leave the current page, and is also able to display simple slideshows. The use of the dark or light background, which dims the page over which the image has been overlaid, also serves to highlight the image being viewed. Lightbox JS by <a href="http://www.huddletogether.com/projects/lightbox/">Lokesh Dhakar</a>.
 Author: Dan Zappone 
 Author URI: http://www.danzappone.com/
-Version: 1.3.1
+Version: 1.3.2
 */
+/*---- 3/27/2009 12:48:47 PM ----*/
 global $post, $content;  // WordPress Globals
 global $g_lightbox_plus_url;
 $g_lightbox_plus_url = WP_PLUGIN_URL.'/lightbox-plus';
@@ -295,7 +296,7 @@ if (!class_exists('wp_lightboxplus')) {
 					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float:right;"> <input name="cmd" type="hidden" value="_donations" /> <input name="business" type="hidden" value="dzappone@gmail.com" /> <input name="item_name" type="hidden" value="Dan Zappone" /> <input name="item_number" type="hidden" value="23SDONWP" /> <input name="no_shipping" type="hidden" value="0" /> <input name="no_note" type="hidden" value="1" /> <input name="currency_code" type="hidden" value="EUR" /> <input name="tax" type="hidden" value="0" /> <input name="lc" type="hidden" value="US" /> <input name="bn" type="hidden" value="PP-DonationsBF" /> <input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" type="image" /> <img src="https://www.paypal.com/en_US/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
 </form>
 					<h4><?php _e('Thank you for downloading and installing Lightbox Plus for WordPress',"lightboxplus"); ?></h4>
-					<?php _e('Like many developers I spend a lot of my spare time working on WordPress plugins and themes and any donation to the cause is appreciated.  I know a lot of other developers do the same and I try to donate to them whenever I can.  As a developer I greatly appreciate any donation you can make to help support further development of quality plugins and themes for WordPress.  In keeping with the name of my site <a href="http://www.23systems.net">23Systems</a> a minimum donation of $2.30 is encouraged but I\'ll gladly accept whatever you feel comfortable with.  <em>You have my sincere thanks and appreciation</em>.',"lightboxplus"); ?>
+					<?php _e('Like many developers I spend a lot of my spare time working on WordPress plugins and themes and any donation to the cause is appreciated.  I know a lot of other developers do the same and I try to donate to them whenever I can.  As a developer I greatly appreciate any donation you can make to help support further development of quality plugins and themes for WordPress.  In keeping with the name of my site <a href="http://www.23systems.net">23Systems</a> a minimum donation of &euro;2.30 is encouraged but I\'ll gladly accept whatever you feel comfortable with. <em>You have my sincere thanks and appreciation</em>.',"lightboxplus"); ?>
 						</div>
 			</div>
 			<?php
